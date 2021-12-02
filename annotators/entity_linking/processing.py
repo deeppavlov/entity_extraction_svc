875d98413c82a07387b65279a314f407213bba78
mtalimanchuk <mtalimanchuk@gmail.com>
2021-12-02 17:49:01 +0300

    entity linking cleanup

--- annotators/entity_linking/processing.py
+++ annotators/entity_linking/processing.py
@@ -1,23 +1,24 @@
 import logging
 import re
 from typing import List, Tuple, Dict, Union
-from collections import defaultdict
 
-import numpy as np
-
-from deeppavlov.core.commands.utils import expand_path
 from deeppavlov.core.common.registry import register
 from deeppavlov.core.models.component import Component
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 GENRES_TEMPLATE = re.compile(
-    r"(\brock\b|heavy metal|\bjazz\b|\bblues\b|\bpop\b|\brap\b|hip hop\btechno\b" r"|dubstep|classic)"
+    r"(\brock\b|heavy metal|\bjazz\b|\bblues\b|\bpop\b|\brap\b|hip hop\btechno\b"
+    r"|dubstep|classic)"
 )
-SPORT_TEMPLATE = re.compile(r"(soccer|football|basketball|baseball|tennis|mma|boxing|volleyball|chess|swimming)")
+SPORT_TEMPLATE = re.compile(
+    r"(soccer|football|basketball|baseball|tennis|mma|boxing|volleyball|chess|swimming)"
+)
 
-genres_dict = {
+GENRES_IDS = {
     "rock": "Q11399",
     "heavy metal": "Q38848",
     "jazz": "Q8341",
@@ -30,7 +31,7 @@ genres_dict = {
     "classic": "Q9730",
 }
 
-sport_dict = {
+SPORT_IDS = {
     "soccer": "Q2736",
     "football": "Q2736",
     "basketball": "Q5372",
@@ -43,48 +44,45 @@ sport_dict = {
     "swimming": "Q31920",
 }
 
+TEMPLATES = {
+    "genres": {"template": GENRES_TEMPLATE, "ids": GENRES_IDS},
+    "sport": {"template": SPORT_TEMPLATE, "ids": SPORT_IDS},
+}
+
 
 @register("entity_linking_preprocessor")
 class EntityLinkingPreprocessor(Component):
     def __init__(self, **kwargs):
         pass
 
-    def __call__(
-        self,
-        entity_substr,
-        template,
-        context
-    ):
+    def __call__(self, entity_substr, template, context):
         long_context = short_context = context
-        logger.error("Contexts:")
-        logger.error(long_context)
-        logger.error(short_context)
-        logger.error(context)
-        # long_context = []
-        # short_context = []
-        # for entity_substr_list, context_list in zip(entity_substr, context):
-        #     last_utt = context_list[-1]
-        #     if (
-        #             len(last_utt) > 1
-        #             and any([entity_substr.lower() == last_utt.lower() for entity_substr in entity_substr_list])
-        #             or any([entity_substr.lower() == last_utt[:-1] for entity_substr in entity_substr_list])
-        #     ):
-        #         context = " ".join(context_list)
-        #     else:
-        #         context = last_utt
-        #     if isinstance(context, list):
-        #         context = " ".join(context)
-        #     if isinstance(last_utt, list):
-        #         short_context = " ".join(last_utt)
-        #     else:
-        #         short_context = last_utt
-        #     long_context.append(context)
-        #     short_context.append(short_context)
-
-        entity_types = [[[] for _ in entity_substr_list] for entity_substr_list in entity_substr]
-        # entity_info = [[{}] for _ in entity_substr]
+        entity_types = [
+            [[] for _ in entity_substr_list] for entity_substr_list in entity_substr
+        ]
 
         return entity_substr, template, long_context, entity_types, short_context
+
+
+def _extract_topic_skill_entities(utt, entity_substr_list, entity_ids_list):
+    found_substr = ""
+    found_id = ""
+    for template_name, template_mappings in TEMPLATES.items():
+        found_template = re.findall(template_mappings["template"], utt)
+        if found_template:
+            template_value = found_template[0]
+            template_value_id = template_mappings["ids"][template_value]
+            is_substr_detected = any(
+                template_value in elem for elem in entity_substr_list
+            )
+            is_id_detected = any(
+                template_value_id in entity_ids for entity_ids in entity_ids_list
+            )
+            if not is_substr_detected or not is_id_detected:
+                found_substr = template_value
+                found_id = template_value_id
+
+    return found_substr, found_id
 
 
 @register("entity_linking_postprocessor")
@@ -93,53 +91,50 @@ class EntityLinkingPostprocessor(Component):
         pass
 
     def __call__(
-        self, entity_ids_batch, conf_batch, tokens_match_conf_batch, entity_substr_batch, short_context_batch
+        self,
+        entity_ids_batch,
+        conf_batch,
+        tokens_match_conf_batch,
+        entity_substr_batch,
+        short_context_batch,
     ):
         entity_info_batch = []
-        for entity_substr_list, entity_ids_list, conf_list, tokens_match_conf_list, context in zip(
-                entity_substr_batch, entity_ids_batch, conf_batch, tokens_match_conf_batch, short_context_batch
+        for (
+            entity_substr_list,
+            entity_ids_list,
+            conf_list,
+            tokens_match_conf_list,
+            context,
+        ) in zip(
+            entity_substr_batch,
+            entity_ids_batch,
+            conf_batch,
+            tokens_match_conf_batch,
+            short_context_batch,
         ):
             entity_info_list = []
             for entity_substr, entity_ids, conf, tokens_match_conf in zip(
-                    entity_substr_list, entity_ids_list, conf_list, tokens_match_conf_list
+                entity_substr_list, entity_ids_list, conf_list, tokens_match_conf_list
             ):
-                entity_info = {}
-                entity_info["entity_substr"] = entity_substr
-                entity_info["entity_ids"] = entity_ids
-                entity_info["confidences"] = [float(elem) for elem in conf]
-                entity_info["tokens_match_conf"] = [float(elem) for elem in tokens_match_conf]
+                entity_info = {
+                    "entity_substr": entity_substr,
+                    "entity_ids": entity_ids,
+                    "confidences": [float(elem) for elem in conf],
+                    "tokens_match_conf": [float(elem) for elem in tokens_match_conf],
+                }
                 entity_info_list.append(entity_info)
-            topic_substr, topic_id = self._extract_topic_skill_entities(context, entity_substr_list, entity_ids_list)
+
+            topic_substr, topic_id = _extract_topic_skill_entities(
+                context, entity_substr_list, entity_ids_list
+            )
             if topic_substr:
-                entity_info = {}
-                entity_info["entity_substr"] = topic_substr
-                entity_info["entity_ids"] = [topic_id]
-                entity_info["confidences"] = [float(1.0)]
-                entity_info["tokens_match_conf"] = [float(1.0)]
+                entity_info = {
+                    "entity_substr": topic_substr,
+                    "entity_ids": [topic_id],
+                    "confidences": [float(1.0)],
+                    "tokens_match_conf": [float(1.0)],
+                }
                 entity_info_list.append(entity_info)
             entity_info_batch.append(entity_info_list)
+
         return entity_info_batch
-
-    def _extract_topic_skill_entities(self, utt, entity_substr_list, entity_ids_list):
-        found_substr = ""
-        found_id = ""
-        found_genres = re.findall(GENRES_TEMPLATE, utt)
-        if found_genres:
-            genre = found_genres[0]
-            genre_id = genres_dict[genre]
-            if all([genre not in elem for elem in entity_substr_list]) or all(
-                    [genre_id not in entity_ids for entity_ids in entity_ids_list]
-            ):
-                found_substr = genre
-                found_id = genre_id
-        found_sport = re.findall(SPORT_TEMPLATE, utt)
-        if found_sport:
-            sport = found_sport[0]
-            sport_id = sport_dict[sport]
-            if all([sport not in elem for elem in entity_substr_list]) or all(
-                    [sport_id not in entity_ids for entity_ids in entity_ids_list]
-            ):
-                found_substr = sport
-                found_id = sport_id
-
-        return found_substr, found_id
