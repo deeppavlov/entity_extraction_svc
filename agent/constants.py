7880062b95869305f9a9ad8fd1c88746d6b3729d
mtalimanchuk <mtalimanchuk@gmail.com>
2022-05-23 22:15:12 +0300

    add ORG tag to type mapping

--- agent/constants.py
+++ agent/constants.py
@@ -1,4 +1,4 @@
 WIKIPEDIA_PAGE_URI_PREFIX = "https://en.wikipedia.org/wiki"
 WIKIPEDIA_FILE_URI_PREFIX = "https://commons.wikimedia.org/wiki/Special:FilePath"
 ONTOLOGY_URI_PREFIX = "https://dbpedia.org/ontology"
-TAG_TO_TYPE_MAP = {"PER": "Agent", "LOC": "Place"}
+TAG_TO_TYPE_MAP = {"PER": "Agent", "LOC": "Place", "ORG": "Organization"}
