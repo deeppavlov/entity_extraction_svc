6187b7ef47a9caf75bd5a2adfc8f7ec17cf10ae3
mtalimanchuk <mtalimanchuk@gmail.com>
2022-05-23 20:28:50 +0300

    rewrite response model

---
+++ agent/constants.py
@@ -0,0 +1,4 @@
+WIKIPEDIA_PAGE_URI_PREFIX = "https://en.wikipedia.org/wiki"
+WIKIPEDIA_FILE_URI_PREFIX = "https://commons.wikimedia.org/wiki/Special:FilePath"
+ONTOLOGY_URI_PREFIX = "https://dbpedia.org/ontology"
+TAG_TO_TYPE_MAP = {"PER": "Agent", "LOC": "Place"}
--- agent/server.py
+++ agent/server.py
@@ -1,10 +1,19 @@
 import logging
-from typing import List
+from datetime import datetime
+from typing import List, Union, Optional
 
 import requests
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, BaseSettings
 from starlette.middleware.cors import CORSMiddleware
+
+from constants import (
+    TAG_TO_TYPE_MAP,
+    WIKIPEDIA_PAGE_URI_PREFIX,
+    ONTOLOGY_URI_PREFIX,
+    WIKIPEDIA_FILE_URI_PREFIX,
+)
+
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -22,9 +31,25 @@ app.add_middleware(
 )
 
 
+class ServerSettings(BaseSettings):
+    entity_extraction_url: str
+    entity_detection_url: str
+    entity_linking_url: str
+    wiki_parser_url: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        env_nested_delimiter = "__"
+
+
 class EntityExtractionAgentRequest(BaseModel):
     """Agent gateway request"""
 
+    text: str
+
+
+class EntityExtractionServiceRequest(BaseModel):
     texts: List[str]
 
 
@@ -33,8 +58,11 @@ class EntityExtractionServiceResponse(BaseModel):
     entity_offsets: List[List[List[int]]]
     entity_ids: List[List[List[str]]]
     entity_tags: List[List[List[str]]]
-    entity_conf: List[List[List[List[float]]]]
+    entity_conf: List[List[List[float]]]
     entity_pages: List[List[List[str]]]
+    image_links: List[List[List[str]]]
+    categories: List[List[List[List[str]]]]
+    first_paragraphs: List[List[List[str]]]
 
     def count_entities(self):
         """Counts number of entities in entity-extraction response
@@ -63,17 +91,23 @@ class EntityExtractionServiceResponse(BaseModel):
     def offsets(self, idx):
         return self.entity_offsets[0][idx]
 
+    def tags(self, idx):
+        return self.entity_tags[0][idx]
+
+    def types(self, idx):
+        types_list = []
+        tags = self.tags(idx)
+
+        for t in tags:
+            try:
+                types_list.append(f"{ONTOLOGY_URI_PREFIX}/{TAG_TO_TYPE_MAP[t]}")
+            except KeyError:
+                pass
+
+        return types_list
+
     def id(self, idx, variety_idx):
         return self.entity_ids[0][idx][variety_idx]
-
-    def tag(self, idx, variety_idx):
-        varieties = self.entity_tags[0][idx]
-        try:
-            variety_tag = varieties[variety_idx]
-        except IndexError:
-            variety_tag = ""
-
-        return variety_tag
 
     def conf(self, idx, variety_idx):
         return self.entity_conf[0][idx][variety_idx]
@@ -81,58 +115,138 @@ class EntityExtractionServiceResponse(BaseModel):
     def page(self, idx, variety_idx):
         return self.entity_pages[0][idx][variety_idx]
 
+    def page_uri(self, idx, variety_idx):
+        page = self.page(idx, variety_idx)
+        return f"{WIKIPEDIA_PAGE_URI_PREFIX}/{page}".replace(" ", "_")
 
-class ExtractedEntityVariety(BaseModel):
+    def lod(self, idx, variety_idx):
+        return {"wikipedia": self.page_uri(idx, variety_idx)}
+
+    def image(self, idx, variety_idx):
+        return self.image_links[0][idx][variety_idx]
+
+    def image_uri(self, idx, variety_idx):
+        image_uri = ""
+        image = self.image(idx, variety_idx)
+        if image:
+            image_uri = f"{WIKIPEDIA_FILE_URI_PREFIX}/{image}".replace(" ", "_")
+
+        return image_uri
+
+    def images(self, idx, variety_idx):
+        images_dict = {}
+        image_uri = self.image_uri(idx, variety_idx)
+        if image_uri:
+            images_dict = {"full": image_uri, "thumbnail": f"{image_uri}?width=300"}
+
+        return images_dict
+
+    def category(self, idx, variety_idx):
+        return self.categories[0][idx][variety_idx]
+
+    def first_paragraph(self, idx, variety_idx):
+        return self.first_paragraphs[0][idx][variety_idx]
+
+
+class EntityAnnotationImage(BaseModel):
+    full: str
+    thumbnail: str
+
+
+class EntityAnnotationLod(BaseModel):
+    wikipedia: str
+
+
+class EntityAnnotation(BaseModel):
+    start: int
+    end: int
+    spot: str
+    confidence: float
     id: str
-    tag: str
-    confidence: List[float]
-    wiki_page: str
+    title: str
+    uri: str
+    abstract: str
+    label: str
+    categories: List[str]
+    tags: List[str]
+    types: List[str]
+    image: Optional[EntityAnnotationImage]
+    lod: EntityAnnotationLod
 
 
-class ExtractedEntity(BaseModel):
-    offset: List[int]
-    substring: str
-    probs: List[ExtractedEntityVariety]
+class EntityAnnotationWithExtras(EntityAnnotation):
+    extras: List[EntityAnnotation]
 
 
 class EntityExtractionAgentResponse(BaseModel):
-    """Agent gateway response"""
+    annotations: List[Union[EntityAnnotation, EntityAnnotationWithExtras]]
+    lang: str
+    timestamp: datetime
 
-    entities: List[ExtractedEntity]
+
+def unpack_annotation(
+    entities: EntityExtractionServiceResponse,
+    entity_idx: int,
+    variety_idx: int,
+):
+    start_offset, end_offset = entities.offsets(entity_idx)
+
+    return EntityAnnotation(
+        start=start_offset,
+        end=end_offset,
+        spot=entities.substr(entity_idx),
+        confidence=entities.conf(entity_idx, variety_idx),
+        id=entities.id(entity_idx, variety_idx),
+        title=entities.page(entity_idx, variety_idx),
+        uri=entities.page_uri(entity_idx, variety_idx),
+        abstract=entities.first_paragraph(entity_idx, variety_idx),
+        label=entities.page(entity_idx, variety_idx),
+        categories=entities.category(entity_idx, variety_idx),
+        tags=entities.tags(entity_idx),
+        types=entities.types(entity_idx),
+        image=EntityAnnotationImage.parse_obj(entities.images(entity_idx, variety_idx)),
+        lod=EntityAnnotationLod.parse_obj(entities.lod(entity_idx, variety_idx)),
+    )
 
 
 def unpack_entity_extraction_service_response(
     entities: EntityExtractionServiceResponse,
+    include_extras: bool = True,
 ):
     readable_entities = []
 
     for idx in range(entities.count_entities()):
-        probs = []
+        top_annotation = unpack_annotation(entities, idx, 0)
 
-        for variety_idx in range(entities.count_entity_variants(idx)):
-            entity_variety = ExtractedEntityVariety(
-                id=entities.id(idx, variety_idx),
-                tag=entities.tag(idx, variety_idx),
-                confidence=entities.conf(idx, variety_idx),
-                wiki_page=entities.page(idx, variety_idx),
+        if include_extras:
+            extra_annotations = []
+
+            for variety_idx in range(1, entities.count_entity_variants(idx)):
+                extra_annotation = unpack_annotation(
+                    entities, idx, variety_idx
+                )
+                extra_annotations.append(extra_annotation)
+
+            full_annotation = EntityAnnotationWithExtras(
+                **top_annotation.dict(), extras=extra_annotations
             )
-            probs.append(entity_variety)
+        else:
+            full_annotation = top_annotation
 
-        entity = ExtractedEntity(
-            offset=entities.offsets(idx),
-            substring=entities.substr(idx),
-            probs=probs,
-        )
-        readable_entities.append(entity)
+        readable_entities.append(full_annotation)
 
-    return EntityExtractionAgentResponse(entities=readable_entities)
+    return EntityExtractionAgentResponse(
+        annotations=readable_entities, lang="en", timestamp=datetime.utcnow()
+    )
+
+
+server_settings = ServerSettings()
 
 
 @app.post("/")
 async def extract(payload: EntityExtractionAgentRequest):
-    response = requests.post(
-        "http://entity-extraction:9103/entity_extraction", json=payload.dict()
-    )
+    request_data = EntityExtractionServiceRequest(texts=[payload.text]).dict()
+    response = requests.post(server_settings.entity_extraction_url, json=request_data)
     entities = response.json()
     logger.error(entities)
     entities = EntityExtractionServiceResponse(**entities)
