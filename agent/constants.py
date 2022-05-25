8e8bc8a31f005a5a050836b72e51b855b32a4ebf
mtalimanchuk <mtalimanchuk@gmail.com>
2022-05-25 22:42:44 +0300

    add unlisted annotations to response, extend tag to type map

--- agent/constants.py
+++ agent/constants.py
@@ -1,4 +1,9 @@
 WIKIPEDIA_PAGE_URI_PREFIX = "https://en.wikipedia.org/wiki"
 WIKIPEDIA_FILE_URI_PREFIX = "https://commons.wikimedia.org/wiki/Special:FilePath"
 ONTOLOGY_URI_PREFIX = "https://dbpedia.org/ontology"
-TAG_TO_TYPE_MAP = {"PER": "Agent", "LOC": "Place", "ORG": "Organization"}
+TAG_TO_TYPE_MAP = {
+    "per": "Agent",
+    "loc": "Place",
+    "org": "Organization",
+    "business": "Organization",
+}
--- agent/server.py
+++ agent/server.py
@@ -120,7 +120,8 @@ class EntityExtractionServiceResponse(BaseModel):
         return f"{WIKIPEDIA_PAGE_URI_PREFIX}/{page}".replace(" ", "_")
 
     def lod(self, idx, variety_idx):
-        return {"wikipedia": self.page_uri(idx, variety_idx)}
+        uri = self.page_uri(idx, variety_idx)
+        return {"wikipedia": uri} if uri else {}
 
     def image(self, idx, variety_idx):
         return self.image_links[0][idx][variety_idx]
@@ -134,7 +135,7 @@ class EntityExtractionServiceResponse(BaseModel):
         return image_uri
 
     def images(self, idx, variety_idx):
-        images_dict = {"full": "", "thumbnail": ""}
+        images_dict = {}
         image_uri = self.image_uri(idx, variety_idx)
         if image_uri:
             images_dict = {"full": image_uri, "thumbnail": f"{image_uri}?width=300"}
@@ -157,10 +158,19 @@ class EntityAnnotationLod(BaseModel):
     wikipedia: str
 
 
-class EntityAnnotation(BaseModel):
+class BaseEntityAnnotation(BaseModel):
     start: int
     end: int
     spot: str
+    tags: List[str]
+    types: List[str]
+
+    @property
+    def has_wikidata(self):
+        return False
+
+
+class EntityAnnotation(BaseEntityAnnotation):
     confidence: float
     id: str
     title: str
@@ -168,18 +178,29 @@ class EntityAnnotation(BaseModel):
     abstract: str
     label: str
     categories: List[str]
-    tags: List[str]
-    types: List[str]
-    image: Optional[EntityAnnotationImage]
-    lod: EntityAnnotationLod
+    image: Optional[dict] = {}
+    lod: Optional[dict] = {}
+
+    @property
+    def has_wikidata(self):
+        return True
+
+
+class BaseEntityAnnotationWithExtras(BaseEntityAnnotation):
+    extras: List[BaseEntityAnnotation]
 
 
 class EntityAnnotationWithExtras(EntityAnnotation):
     extras: List[EntityAnnotation]
 
 
+AnyBaseAnnotation = Union[BaseEntityAnnotation, BaseEntityAnnotationWithExtras]
+AnyAnnotation = Union[EntityAnnotation, EntityAnnotationWithExtras]
+
+
 class EntityExtractionAgentResponse(BaseModel):
-    annotations: List[Union[EntityAnnotation, EntityAnnotationWithExtras]]
+    annotations: Optional[List[AnyAnnotation]] = []
+    unlisted_annotations: Optional[List[AnyBaseAnnotation]] = []
     lang: str
     timestamp: datetime
 
@@ -189,31 +210,54 @@ def unpack_annotation(
     entity_idx: int,
     variety_idx: int,
 ):
-    start_offset, end_offset = entities.offsets(entity_idx)
+    """
+    Creates an Annotation data object
 
-    return EntityAnnotation(
-        start=start_offset,
-        end=end_offset,
-        spot=entities.substr(entity_idx),
-        confidence=entities.conf(entity_idx, variety_idx),
-        id=entities.id(entity_idx, variety_idx),
-        title=entities.page(entity_idx, variety_idx),
-        uri=entities.page_uri(entity_idx, variety_idx),
-        abstract=entities.first_paragraph(entity_idx, variety_idx),
-        label=entities.page(entity_idx, variety_idx),
-        categories=entities.category(entity_idx, variety_idx),
-        tags=entities.tags(entity_idx),
-        types=entities.types(entity_idx),
-        image=EntityAnnotationImage.parse_obj(entities.images(entity_idx, variety_idx)),
-        lod=EntityAnnotationLod.parse_obj(entities.lod(entity_idx, variety_idx)),
-    )
+    Args:
+        entities: entities from entity-extraction service
+        entity_idx: entity index
+        variety_idx: entity variety index
+
+    Returns:
+        annotation object
+
+    """
+    start_offset, end_offset = entities.offsets(entity_idx)
+    wikidata_id = entities.id(entity_idx, variety_idx)
+
+    if wikidata_id:
+        return EntityAnnotation(
+            start=start_offset,
+            end=end_offset,
+            spot=entities.substr(entity_idx),
+            confidence=entities.conf(entity_idx, variety_idx),
+            id=wikidata_id,
+            title=entities.page(entity_idx, variety_idx),
+            uri=entities.page_uri(entity_idx, variety_idx),
+            abstract=entities.first_paragraph(entity_idx, variety_idx),
+            label=entities.page(entity_idx, variety_idx),
+            categories=entities.category(entity_idx, variety_idx),
+            tags=entities.tags(entity_idx),
+            types=entities.types(entity_idx),
+            image=entities.images(entity_idx, variety_idx),
+            lod=entities.lod(entity_idx, variety_idx),
+        )
+    else:
+        return BaseEntityAnnotation(
+            start=start_offset,
+            end=end_offset,
+            spot=entities.substr(entity_idx),
+            tags=entities.tags(entity_idx),
+            types=entities.types(entity_idx),
+        )
 
 
 def unpack_entity_extraction_service_response(
     entities: EntityExtractionServiceResponse,
     include_extras: bool = True,
 ):
-    readable_entities = []
+    unlisted_annotations = []
+    listed_annotations = []
 
     for idx in range(entities.count_entities()):
         top_annotation = unpack_annotation(entities, idx, 0)
@@ -222,21 +266,28 @@ def unpack_entity_extraction_service_response(
             extra_annotations = []
 
             for variety_idx in range(1, entities.count_entity_variants(idx)):
-                extra_annotation = unpack_annotation(
-                    entities, idx, variety_idx
-                )
+                extra_annotation = unpack_annotation(entities, idx, variety_idx)
                 extra_annotations.append(extra_annotation)
 
+        else:
+            extra_annotations = None
+
+        if top_annotation.has_wikidata:
             full_annotation = EntityAnnotationWithExtras(
                 **top_annotation.dict(), extras=extra_annotations
             )
+            listed_annotations.append(full_annotation)
         else:
-            full_annotation = top_annotation
-
-        readable_entities.append(full_annotation)
+            full_annotation = BaseEntityAnnotationWithExtras(
+                **top_annotation.dict(), extras=extra_annotations
+            )
+            unlisted_annotations.append(full_annotation)
 
     return EntityExtractionAgentResponse(
-        annotations=readable_entities, lang="en", timestamp=datetime.utcnow()
+        annotations=listed_annotations,
+        unlisted_annotations=unlisted_annotations,
+        lang="en",
+        timestamp=datetime.utcnow(),
     )
 
 
