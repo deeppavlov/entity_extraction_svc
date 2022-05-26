7f2531c1ea9127e62a96de812d54de02848aa182
Daniel Kornev <daniel@zetuniverse.com>
2022-05-26 13:27:53 +0300

    Update constants.py

--- agent/constants.py
+++ agent/constants.py
@@ -2,7 +2,7 @@ WIKIPEDIA_PAGE_URI_PREFIX = "https://en.wikipedia.org/wiki"
 WIKIPEDIA_FILE_URI_PREFIX = "https://commons.wikimedia.org/wiki/Special:FilePath"
 ONTOLOGY_URI_PREFIX = "https://dbpedia.org/ontology"
 TAG_TO_TYPE_MAP = {
-    "per": "Agent",
+    "per": "Person",
     "loc": "Place",
     "org": "Organisation",
     "business": "Organisation",
