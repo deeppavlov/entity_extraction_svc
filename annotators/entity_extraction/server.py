4d3a94e27e76ae78f57a571b693342619cbec922
Дмитрий Евсеев <dmitrij.euseew@yandex.ru>
2022-05-29 14:40:51 +0300

    misc thres

--- annotators/entity_extraction/server.py
+++ annotators/entity_extraction/server.py
@@ -32,11 +32,13 @@ app.add_middleware(
 ner_config_name = os.getenv("NER_CONFIG")
 el_config_name = os.getenv("EL_CONFIG")
 include_misc = bool(int(os.getenv("INCLUDE_MISC", "0")))
+misc_thres = float(os.getenv("MISC_THRES", "0.88"))
 
 ner_config = parse_config(ner_config_name)
 entity_detection_config_name = ner_config['chainer']['pipe'][1]['ner']['config_path']
 entity_detection = json.load(open(entity_detection_config_name, 'r'))
 entity_detection["chainer"]["pipe"][6]["include_misc"] = include_misc
+entity_detection["chainer"]["pipe"][6]["misc_thres"] = misc_thres
 json.dump(entity_detection, open(entity_detection_config_name, 'w'), indent=2)
 
 logger.info(f"ner_config {ner_config['chainer']['pipe'][1]['ner']}")
