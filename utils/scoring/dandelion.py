3598771fad9bed39c57b7bba15c2e15d2132415a
mtalimanchuk <mtalimanchuk@gmail.com>
2022-06-28 20:42:46 +0300

    update notebook with scoring

--- utils/scoring/dandelion.py
+++ utils/scoring/dandelion.py
@@ -1,16 +1,32 @@
+from dataclasses import dataclass
+from typing import Optional
+
 from environs import Env
 from requests import Session
-from tqdm import tqdm
 
 ENDPOINT = "https://api.dandelion.eu/datatxt/nex/v1/"
 TEXT_TYPES = ["text", "html", "html_fragment"]
-INCLUDE_OPTIONS = ["types", "categories", "abstract", "image", "lod", "alternate_labels"]
+INCLUDE_OPTIONS = [
+    "types",
+    "categories",
+    "abstract",
+    "image",
+    "lod",
+    "alternate_labels",
+]
 LANG_OPTIONS = ["de", "en", "es", "fr", "it", "pt", "ru", "auto"]
 
 env = Env()
 env.read_env()
 
 DANDELION_TOKEN = env.str("DANDELION_TOKEN")
+
+
+@dataclass
+class DandelionResponse:
+    status_code: int
+    data: Optional[dict]
+    error: Optional[str]
 
 
 def extract_dandelion(
@@ -22,19 +38,25 @@ def extract_dandelion(
     min_confidence: float = 0.6,
     min_length: int = 2,
     include: list = None,
+    token: str = None,
 ):
-    """Extract entities from a text, html or html fragment.
+    """Extracts entities from a text, html or html fragment.
     Refer to https://dandelion.eu/docs/api/datatxt/nex/v1/#parameters for details
 
-    :param session: requests session
-    :param text: text to extract from
-    :param text_type: "text" for plain text, "html" for full html documents or "html_fragment" for html snippets
-    :param lang: explicitly set text language. Detected automatically by default
-    :param top_entities: return only N most important entities if > 0
-    :param min_confidence: return only entities with confidence above threshold
-    :param min_length: return only entities with min length > N
-    :param include: list of additional info keys which should be returned
-    :return:
+    Args:
+        session: requests session
+        text: text to extract from
+        text_type: "text" for plain text, "html" for full html documents or "html_fragment" for html snippets
+        lang: explicitly set text language. Detected automatically by default
+        top_entities: return only N most important entities if > 0
+        min_confidence: return only entities with confidence above threshold
+        min_length: return only entities with min length > N
+        include: list of additional info keys which should be returned
+        token: Dandelion token. If None, loads it from DANDELION_TOKEN env.
+            You can export it via terminal: ``export DANDELION_TOKEN="yourtokenhere"``
+
+    Returns:
+
     """
 
     if text_type not in TEXT_TYPES:
@@ -46,6 +68,12 @@ def extract_dandelion(
     if lang and lang not in LANG_OPTIONS:
         raise Exception(f"Choose lang value from {', '.join(LANG_OPTIONS)}")
 
+    if not (token or DANDELION_TOKEN):
+        raise Exception(
+            f"No token found. Provide it via 'export DANDELION_TOKEN=\"yourtokenhere\"'"
+            " or place it inside .env in repo root"
+        )
+
     url_kwargs = {
         text_type: text,
         "lang": lang or "auto",
@@ -53,27 +81,16 @@ def extract_dandelion(
         "min_confidence": min_confidence,
         "min_length": min_length,
         "include": ",".join(include) if include else "",
-        "token": DANDELION_TOKEN,
+        "token": token if token else DANDELION_TOKEN,
     }
 
     response = session.get(ENDPOINT, params=url_kwargs)
-    data = response.json()
+    data = error = None
+    status_code = response.status_code
 
-    return data
+    try:
+        data = response.json()
+    except Exception as e:
+        error = f"{type(e)}: {e}"
 
-
-def extract_batch_dandelion(texts: list, **kwargs):
-    results = []
-
-    with Session() as sess:
-        for text in tqdm(texts):
-            entities = extract_dandelion(sess, text, **kwargs)
-            results.append(entities)
-
-    return results
-
-
-# if __name__ == '__main__':
-    # with Session() as sess:
-    #     entities = extract_dandelion(sess, "Mona Lisa is located in Louvre")
-    #     print(entities)
+    return DandelionResponse(status_code=status_code, data=data, error=error)
